089e849b14bfbad3fb89a22cbd055343a7640a73
CasMagnus1 <cas.magnus@student.kuleuven.be>
2020-03-13 18:28:08 +0100

    eerste request

--- src/code/Client.java
+++ src/code/Client.java
@@ -16,23 +16,26 @@ public class Client {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             
-            if (httpCommand.equals("GET")) {
-            	//send get request
-            }
-            if (httpCommand.equals("HEAD")) {
-            	//send head request
-            }
-            if (httpCommand.equals("PUT")) {
-            	//send put request
-            }
-            if (httpCommand.equals("POST")) {
+        	//send request
+        	out.println(httpCommand + " " + uri.getPath() + " HTTP/1.1");
+        	out.println("Host: " + uri.getHost());
+        	out.println("Accept-Language: " + language);
+        	out.println("");
+        	
+            if (httpCommand.equals("POST") || httpCommand.equals("PUT")) {
+            	//include body
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 System.out.println("type what you want to send: ");
                 String stringToSend = in.readLine();
-                //send post request met stringToSend als body
             }
-            
-            
+        	
+        	//receive response
+    		String newLine;
+			while((newLine = reader.readLine()) != null) {
+				System.out.println(newLine);
+			}
+			
+           
 		}
 		catch(URISyntaxException ex) {
 			System.out.println("URI expection: " + ex.getMessage());
@@ -41,7 +44,7 @@ public class Client {
             System.out.println("Server not found: " + ex.getMessage());
         }
 		catch (IOException ex) {
-            System.out.println("I/O error: " + ex.getMessage());
+            System.out.println("I/O or readLine error: " + ex.getMessage());
         }
     }
 }
