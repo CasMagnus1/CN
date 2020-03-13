579fb4e94cab10084ea94c108692fb68fe1a0bda
CasMagnus1 <cas.magnus@student.kuleuven.be>
2020-03-13 15:56:26 +0100

    URI parsing toegevoegd

--- src/code/Client.java
+++ src/code/Client.java
@@ -6,20 +6,24 @@ import java.net.*;
 public class Client {
 
 	public static void main(String[] args) {
-		String httpCommand = args[0];
-		String URI = args[1];
-        int port = Integer.parseInt(args[2]);
-        String language = args[3];
- 
-        try {
+		try {
+			String httpCommand = args[0];
+			URI uri = new URI(args[1]);
+	        int port = Integer.parseInt(args[2]);
+	        String language = args[3];
+	        
         	//URI nog parsen
-        	Socket socket = new Socket(URI, port);
+        	Socket socket = new Socket(uri.getHost(), port);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            
-        } catch (UnknownHostException ex) {
+		}
+		catch(URISyntaxException ex) {
+			System.out.println("URI expection: " + ex.getMessage());
+		}        
+        catch (UnknownHostException ex) {
             System.out.println("Server not found: " + ex.getMessage());
-        } catch (IOException ex) {
+        }
+		catch (IOException ex) {
             System.out.println("I/O error: " + ex.getMessage());
         }
     }
