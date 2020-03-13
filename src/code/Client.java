cb3cfe3fe3b52aaf47541941fa6ba94657af1f08
CasMagnus1 <cas.magnus@student.kuleuven.be>
2020-03-13 11:16:56 +0100

    classes aangemaakt

---
+++ src/code/Client.java
@@ -0,0 +1,8 @@
+package code;
+
+public class Client {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/code/Server.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
