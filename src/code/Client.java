fa8f5069076445c13bac970263434a20fd1ab5fb
CasMagnus1 <cas.magnus@student.kuleuven.be>
2020-03-13 11:34:15 +0100

    begin

--- src/code/Client.java
+++ src/code/Client.java
@@ -1,8 +1,26 @@
 package code;
 
+import java.io.*;
+import java.net.*;
+
 public class Client {
 
 	public static void main(String[] args) {
-	}
-
+		String httpCommand = args[0];
+		String URI = args[1];
+        int port = Integer.parseInt(args[2]);
+        String language = args[3];
+ 
+        try {
+        	//URI nog parsen
+        	Socket socket = new Socket(URI, port);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
 }
--- src/code/Server.java
+++ src/code/Server.java
@@ -1,10 +1,29 @@
 package code;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int port = 3000;
+        
+        try {
+        	ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server is listening on port " + port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+ 
+                System.out.println("New client connected");
+ 
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+        }
 	}
 
 }
